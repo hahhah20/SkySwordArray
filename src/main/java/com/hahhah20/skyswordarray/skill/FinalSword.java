45a24415e4759438bc5efb6fd4132305ea27abb1
hahhah20 <125357214+hahhah20@users.noreply.github.com>
2026-08-26 13:23:38 +0800

    Add files via upload

--- src/main/java/com/hahhah20/skyswordarray/skill/FinalSword.java
+++ src/main/java/com/hahhah20/skyswordarray/skill/FinalSword.java
@@ -1,13 +1,29 @@
 package com.hahhah20.skyswordarray.skill;
 
+
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class FinalSword {
-    public static void strike(Player player, Location loc){
-        new SwordRain(player.getServer()
-            .getPluginManager()
-            .getPlugin("SkySwordArray"))
-            .execute(player,loc);
+
+
+    public static void strike(
+            JavaPlugin plugin,
+            Player player,
+            Location loc
+    ){
+
+
+        new SwordRain(plugin)
+                .execute(
+                        player,
+                        loc
+                );
+
+
     }
+
+
 }
